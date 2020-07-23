3d7464d7977271d3fc82d868fa9a52d6b0f3d297
brianwlego <legobw@gmail.com>
2020-07-23 16:28:59 -0400

    fixed Student class

--- app/student.rb
+++ app/student.rb
@@ -5,7 +5,7 @@ class Student < ActiveRecord::Base
     student.first_name + " " + student.last_name
   end
   def self.all_in_grade(grade)
-    Student.where("grade_level = ?", params[:grade])
+    Student.select {|s| s.grade_level == grade}
   end
 
 end
